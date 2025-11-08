121837f6ba1a301e5226d7600e5cc04465b5575b
FerdiRiyantoo <ferdiriyanto00@gmail.com>
2025-11-08 07:17:12 +0700

    menambahkan File insert.php

---
+++ insert.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = ""; 
+$dbname = "db_film";
+$namatabel = "film"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi Gagal: " . $conn->connect_error);
+}
+
+$judul_Film = "Agak Laen";
+$pemeran = "Indra Jegel";
+$tahun_tayang = 2024;
+
+
+$sql = "INSERT INTO $film (judul, pemeran, tahun_terbit)
+        VALUES ('$judul_buku', '$penulis_buku', $tahun_tayang)";
+
+echo "<h1>Menjalankan Fitur Tambah Data...</h1>";
+if ($conn->query($sql) === TRUE) {
+    echo "<p>Data baru berhasil ditambahkan ke tabel '$film'!</p>";
+} else {
+    echo "<p>Error: " . $sql . "<br>" . $conn->error . "</p>";
+}
+
+$conn->close();
+?>
